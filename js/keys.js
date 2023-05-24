27ceeb9531ca2419dcfd42cbf641ada020678f2b
darryn.campbell <darryn.campbell@pubnub.com>
2023-05-24 15:29:36 +0100

    added empty keys file

---
+++ js/keys.js
@@ -0,0 +1,6 @@
+/**
+ * Populate this file with both your PubNub keys
+ * Please see the ReadMe file for details of where to obtain keys
+ */
+const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
+const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
