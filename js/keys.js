de99ce029cdc00798b5353229855b4387a35b637
Jay Greentree <JayGreentree@users.noreply.github.com>
2025-02-11 22:21:41 -0600

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -2,5 +2,5 @@
  * Populate this file with both your PubNub keys
  * Please see the ReadMe file for details of where to obtain keys
  */
-const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
+const publish_key = 'pub-c-15fc919e-3d3c-4922-bc6d-5232e7da0569' //  ENTER YOUR PUBLISH KEY HERE
+const subscribe_key = 'sub-c-28db1648-85a4-4cc5-9abc-b749842134ba' //  ENTER YOUR SUBSCRIBE KEY HERE
